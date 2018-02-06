326c0bdd933fc566872818791bed3829a7de45b5
ALondono <armando.londono@yuxipacific.com>
2018-02-05 20:00:31 -0500

    testing image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-selector: 'app-root',
-templateUrl: './app.component.html',
-styleUrls: [ './app.component.css' ]
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
-
 export class AppComponent {
-title = 'My Application';
-author = 'Armando Londoño';
-count = 0;
-imgSource = './../favicon.ico';
-enabled = false;
+	title = 'My Application';
+	author = 'Armando Londoño';
+	count = 0;
+	//imgSource = './../favicon.ico';
+	imgSource = './favicon.ico';
+	enabled = false;
 }
