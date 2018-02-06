a586ed0bca9d4547d7b970f38e744a20ac51a828
ALondono <armando.londono@yuxipacific.com>
2018-02-05 19:51:13 -0500

    Finishing exercise #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: [ './app.component.css' ]
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent {
-	title = 'My Application';
-	author = 'Armando Londoño';
+title = 'My Application';
+author = 'Armando Londoño';
+count = 0;
+imgSource = './../favicon.ico';
+enabled = false;
 }
